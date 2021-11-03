0c431b9cb14683916f4848bd4887f6c81fc7820d
raychannudam <62501505+raychannudam@users.noreply.github.com>
2021-11-03 08:06:03 +0700

    Created class Payslip

---
+++ Payslips-Sender/Payslip.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payslips_Sender
+{
+    class Payslip
+    {
+        //Data-There are two information in this class such as payslip templete path and datasource path
+        private string _Temp_Path; //variable to store the templete path as a string.
+        private string _Data_Path; //variable to store the datasource path as a string.
+
+        //Constructor-There are two attribute or parameter in the constructor
+        public Payslip(string tempPath, string dataPath)
+        {
+            this._Temp_Path = tempPath;
+            this._Data_Path = dataPath;
+        }
+
+        //Property-There are two property in this class
+        public string tempPath
+        {
+            get { return _Temp_Path; }
+            set { value = tempPath; }
+        }
+        public string dataPath
+        {
+            get { return _Data_Path; }
+            set { value = dataPath; }
+        }
+    }
+}
